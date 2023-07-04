4af91411d7ec1d9b002183131a110abb92ec6377
Julian198939 <love198939@yahoo.com>
2023-07-04 17:01:29 +0800

    changed

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.get("/", (req, res) => {
   res.send("Hello server is working");
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
   console.log(`App started listening at port ${PORT}`);
